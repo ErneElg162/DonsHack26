e1b1193dc78f5a1142d939ead9b3dc760a0f62cf
Ernesto Elguezabal <ernestoelg50@gmail.com>
2026-03-07 01:21:25 -0800

    Created a working test controller

--- TestControls.gd
+++ TestControls.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 var rb: RigidBody2D
+var null_path: NodePath = NodePath("")
 
 var cp1: Node2D
 var joint1: Joint2D
+
 @export var hidden1: StaticBody2D
+var hidden1_path: NodePath
 
 var cp2: Node2D
 
@@ -15,16 +18,27 @@ func _ready() -> void:
 	
 	cp1 = get_node("ControlPoint1")
 	joint1 = get_node("Joint1")
+	hidden1_path = hidden1.get_path()
+	
+	#joint1.set_physics_process(false)
+	#hidden1.set_process(false)
 	
 	cp2 = get_node("ControlPoint2")
-	
+
 
 func cling():
-	hidden1.global_position = cp1.global_position
-	joint1.node_b = hidden1.get_path()
+	joint1.node_b = hidden1_path
+	#hidden1.global_position = cp1.global_position
+	#joint1.global_position = hidden1.global_position
+	
+	#joint1.set_physics_process(true)
+	#hidden1.set_process(true)
 
 func uncling():
 	joint1.node_b = NodePath("")
+	#pass
+	#joint1.set_physics_process(false)
+	#hidden1.set_process(false)
 
 
 func force():
@@ -36,9 +50,11 @@ func _process(delta: float) -> void:
 		force()
 	
 	if Input.is_action_just_pressed("Cling"):
-		print_debug("CLINGED")
 		cling()
 		
 	if Input.is_action_just_released("Cling"):
-		print_debug("UNCLINGED")
-		uncling()
+		uncling() 
+		
+	if Input.is_action_just_pressed("Down"):
+		print_debug("Hidden: ", hidden1.global_position)
+		print_debug("Joint: ", get_node(joint1.node_b).global_position)
