2557cdfc111516f02765f75a9f51b257b4074059
Ernesto Elguezabal <ernestoelg50@gmail.com>
2026-03-07 04:13:06 -0800

    Cling only when touching surface

---
+++ Player.gd
@@ -0,0 +1,8 @@
+class_name Player
+
+var cp: Node2D
+var joint: Joint2D
+var anchor: NodePath
+var can_cling: bool
+var clinging: bool
+var mask: Area2D
--- TestControls.gd
+++ TestControls.gd
@@ -1,60 +1,118 @@
 extends Node2D
 
+
 var rb: RigidBody2D
 var null_path: NodePath = NodePath("")
+var impulse = 300
 
-var cp1: Node2D
-var joint1: Joint2D
+@export var p1_anchor: StaticBody2D
+@export var p2_anchor: StaticBody2D
+@export var p3_anchor: StaticBody2D
 
-@export var hidden1: StaticBody2D
-var hidden1_path: NodePath
-
-var cp2: Node2D
-
-var boop = Vector2(0, -200);
+var p1: Player = Player.new()
+var p2: Player = Player.new()
+var p3: Player = Player.new()
 
 func _ready() -> void:
 	rb = get_node(".")
 	
-	cp1 = get_node("ControlPoint1")
-	joint1 = get_node("Joint1")
-	hidden1_path = hidden1.get_path()
+	p1.cp = get_node("ControlPoint1")
+	p1.joint = get_node("Joint1")
+	p1.anchor = p1_anchor.get_path()
+	p1.can_cling = false;
+	p1.clinging = false
+	p1.mask = p1.cp.get_child(0)
 	
-	#joint1.set_physics_process(false)
-	#hidden1.set_process(false)
+	p2.cp = get_node("ControlPoint2")
+	p2.joint = get_node("Joint2")
+	p2.anchor = p2_anchor.get_path()
+	p2.can_cling = false;
+	p2.clinging = false
+	p2.mask = p2.cp.get_child(0)
 	
-	cp2 = get_node("ControlPoint2")
+	p3.cp = get_node("ControlPoint3")
+	p3.joint = get_node("Joint3")
+	p3.anchor = p3_anchor.get_path()
+	p3.can_cling = false;
+	p3.clinging = false
+	p3.mask = p3.cp.get_child(0)
 
 
-func cling():
-	joint1.node_b = hidden1_path
-	#hidden1.global_position = cp1.global_position
-	#joint1.global_position = hidden1.global_position
-	
-	#joint1.set_physics_process(true)
-	#hidden1.set_process(true)
+func cling(player: Player):
+	if player.can_cling:
+		player.joint.node_b = player.anchor
+		player.clinging = true
 
-func uncling():
-	joint1.node_b = NodePath("")
-	#pass
-	#joint1.set_physics_process(false)
-	#hidden1.set_process(false)
+func uncling(player: Player):
+	player.joint.node_b = null_path
+	player.clinging = false
 
 
-func force():
-	rb.apply_impulse(boop, cp2.position)
+func force(player: Player, cc: bool):
+	if !player.clinging:
+		var imp = impulse * player.cp.get_relative_transform_to_parent(player.cp.get_parent()).x.normalized()
+		
+		if !cc:
+			imp *= -1
+		
+		rb.apply_impulse(imp, player.cp.position)
+
+func overlap_clingable(player: Player):
+	if player.mask.get_overlapping_bodies().size() > 0:
+		player.can_cling = true
+		
+	else:
+		player.can_cling = false
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Up"):
-		force()
+	#P1
+	if Input.is_action_just_pressed("P1_CC"):
+		force(p1, true)
+		
+	if Input.is_action_just_pressed("P1_C"):
+		force(p1, false)
 	
-	if Input.is_action_just_pressed("Cling"):
-		cling()
+	if Input.is_action_just_pressed("P1_S"):
+		cling(p1)
 		
-	if Input.is_action_just_released("Cling"):
-		uncling() 
+	if Input.is_action_just_released("P1_S"):
+		uncling(p1)
 		
-	if Input.is_action_just_pressed("Down"):
-		print_debug("Hidden: ", hidden1.global_position)
-		print_debug("Joint: ", get_node(joint1.node_b).global_position)
+	#P2
+	if Input.is_action_just_pressed("P2_CC"):
+		force(p2, true)
+		
+	if Input.is_action_just_pressed("P2_C"):
+		force(p2, false)
+	
+	if Input.is_action_just_pressed("P2_S"):
+		cling(p2)
+		
+	if Input.is_action_just_released("P2_S"):
+		uncling(p2)
+		
+	#P3
+	if Input.is_action_just_pressed("P3_CC"):
+		force(p3, true)
+		
+	if Input.is_action_just_pressed("P3_C"):
+		force(p3, false)
+	
+	if Input.is_action_just_pressed("P3_S"):
+		cling(p3)
+		
+	if Input.is_action_just_released("P3_S"):
+		uncling(p3)
+		
+
+	#DEBUG
+	if Input.is_action_just_pressed("DEBUG"):
+		print_debug("P1: ", p1.can_cling)
+		print_debug("P2: ", p2.can_cling)
+		print_debug("P3: ", p3.can_cling)
+	
+	#Set can_cling
+	overlap_clingable(p1)
+	overlap_clingable(p2)
+	overlap_clingable(p3)
