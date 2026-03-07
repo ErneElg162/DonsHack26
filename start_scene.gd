644beca2d7954592b8003c66e5fee43647f5af37
Tianqi Min <tmin2@dons.usfca.edu>
2026-03-07 11:37:02 -0800

    add a scene

--- start_scene.gd
+++ start_scene.gd
@@ -1,7 +1,15 @@
 extends Node2D
 var game_scene_path = "res://sceen_1.tscn"
+@export var start_button : Button
+
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_tree().paused = true
+	start_button.visible = true
+	start_button.pressed.connect(_on_start_button_pressed)
 
 func _on_start_button_pressed():
+	get_tree().paused = false
 	get_tree().change_scene_to_file(game_scene_path)
 	
-	get_tree().paused = false
